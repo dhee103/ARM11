a29a7cf64f6bf5ade275f30ac55186a95183cf57
dsg115 <dsg115>
2016-05-27 15:18:57 +0100

    no message

--- src/emulate/definitions.h
+++ src/emulate/definitions.h
@@ -1,17 +1,19 @@
 #ifndef DEFS
 #define DEFS
-/* This file includes defintions that will be used for the emulator */
+/* This file includes definitions that will be used for the emulator */
 
 /* Memory & Registers */
+#include <stdlib.h>
+#include <memory.h>
+
 #define BYTES_IN_WORD 4
 #define BITS_IN_WORD 32
-#define MEMORY_SIZE 65536
+#define MEM_SIZE 65536
 #define NUM_REGISTERS 17
-#define SP 13
-#define LR 14
-#define PC 15
-#define CPSR 16
+#define PC 13
+#define CPSR 14
 #define PC_AHEAD_BYTES 8
+#define BITS_IN_BYTE 8
 
 /* Bit position */
 #define MS_BIT 31
@@ -25,21 +27,30 @@
 #define P_BIT 24
 #define U_BIT 23
 #define L_BIT 20
+#define isBranch_bit 27
+#define isSDT_bit 26
+#define isMult_bit 25
 
 /* masks & shifts*/
 #define condMask 0xf
-#define condMask_shift 28
-#define opcodeMask 0xf
-#define opcodeMask_shift 21
+#define condMask_shift 4
+#define opcodeMask 0x7
+#define oneMask 0x1 << 3
+#define opcodeMask_shift 13
+#define multMask 0x9 << 28
+#define BRANCHSHIFT 2
+#define check_bit31 0x80000000
+#define check_bit0 0x00000001
+#define check_bit4 0x01
+#define check_shift_type 0x06
+#define check_shift_value 0xF8
+#define check_Rs 0xf0
+#define check_bit0_7 0x000000FF
+#define check_bit8_11 0x00000F00
+#define check_bit0_3 0x0000000F
+#define check_bit4_11 0x00000FF0
+#define FULLSHIFT 31
 
-/* Enum named INSTR for instruction type */
-typedef enum {
-	DATA_PROC,
-	MULTIPLY,
-	SINGLE_DT,
-	BRANCH,
-	END
-} INSTR;
 
 /* Enum named COND for instruction condition */
 typedef enum {
@@ -76,7 +87,6 @@ typedef enum {
 
 /* information held by the decoded instruction */
 typedef struct decoded_instruction {
-	INSTR instruction;
 	COND condition;
 	OPCODE opcode;
 	SHIFT shift;
@@ -92,20 +102,63 @@ typedef struct decoded_instruction {
 	uint32_t rs;
 	uint32_t rm;
 	uint32_t offset;
+    uint32_t operand2;
 } decoded_instr;
+
+typedef struct cpsrFlag {
+	int nbit;
+	int zbit;
+	int cbit;
+	int vbit;
+}cpsr;
 	
 typedef struct state {
-	decoded_instr decoded;
+	decoded_instr *decoded;
+//    uint32_t *instr;
 	uint32_t *reg;
 	uint8_t *memory;
+    uint32_t pc;
+    cpsr *cpsrFlag;
 	int isFetched;
 	int isDecoded;
 	int isExecuted;
 } state;
 
+extern uint32_t regs[NUM_REGISTERS];
+
 typedef struct shift_output {
 	uint32_t data;
 	int carry;
 } shift_out;
+
+state *newState(void) {
+    state *st = malloc(sizeof(state));
+    st->decoded = malloc(sizeof(struct decoded_instruction));
+    st->memory = malloc(MEM_SIZE*sizeof(uint8_t));
+    memset(st->memory,0,sizeof(uint8_t));
+    st->reg = malloc(NUM_REGISTERS*sizeof(uint32_t));
+    memset(st->reg,0,sizeof(uint32_t));
+    st->isDecoded = 0;
+    st->isFetched = 0;
+    return st;
+}
+
+void freeState(state *st) {
+    if (st != NULL) {
+        if (st->decoded != NULL) {
+            free(st->decoded);
+        }
+
+        if (st->reg != NULL) {
+            free(st->reg);
+        }
+
+        if (st->memory != NULL) {
+            free(st->memory);
+        }
+
+        free(st);
+    }
+}
 	
 #endif
--- src/emulate/getCodes.c
+++ src/emulate/getCodes.c
@@ -1,24 +1,118 @@
 #include <stdio.h>
 #include <stdint.h>
 #include "definitions.h"
+#include "getCodes.h"
+#include "instructions.c"
+#include "instructionCycle.c"
 
-int getCond(uint32_t input) {
-        input = (input & (condMask << condMask_shift)) >> condMask_shift;
-        printf("%u\n",input);
-        return (int)input;
+int getCond(uint32_t instruction) {
+	instruction = (instruction & (condMask << condMask_shift)) >> condMask_shift;
+	return (int)instruction;
 }
 
-int getOpcode(uint32_t input) {
-	input = (input & (opcodeMask << opcodeMask_shift)) >> opcodeMask_shift;
-	printf("%u\n",input);
-	return (int)input;
+int checkCond(state *st) {
+    uint32_t instruction = getInstruction(st);
+    switch(getCond(instruction)) {
+        case EQ :
+            return st->cpsrFlag->zbit;
+        case NE :
+            return !st->cpsrFlag->zbit;
+        case GE :
+            return st->cpsrFlag->nbit == st->cpsrFlag->vbit;
+        case LT :
+            return st->cpsrFlag->nbit != st->cpsrFlag->vbit;
+        case GT :
+            return !st->cpsrFlag->zbit & (st->cpsrFlag->nbit == st->cpsrFlag->vbit);
+        case LE :
+            return st->cpsrFlag->zbit | (st->cpsrFlag->nbit != st->cpsrFlag->vbit);
+        case AL :
+            return EXIT_SUCCESS;
+        default:
+            return EXIT_FAILURE;
+    }
 }
 
-int main(void) {
-        uint32_t input = 0xE2345680;
-	uint32_t alt = 0xEEEEEEEE;
-        getCond(input);
-	getOpcode(alt);
-        return 0;
+int getOpcode(uint32_t instruction) {
+	instruction = (instruction & (opcodeMask << opcodeMask_shift)) >> opcodeMask_shift;
+	if ((instruction & 1) == 1) {
+		instruction |= oneMask;
+	}
+	return (int)instruction;
 }
-                                                 
+
+int bitAccess(int index, uint32_t instruction) {
+
+    if (0 <= index && index <= 7) {
+        index = index + 24;
+    } else if (8 <= index && index <= 15) {
+        index = index + 8;
+    } else if (16 <= index && index <= 23) {
+        index = index - 8;
+    } else if (24 <= index && index <= 31) {
+        index = index - 24;
+    }
+
+    uint32_t mask = 1;
+    mask = mask << index;
+
+    return (mask & instruction) != 0;
+
+}
+
+int isMult(uint32_t instruction) {
+//	uint32_t multMask = 0x9;
+//	multMask <<= 28;
+	return ((instruction & multMask) == multMask);
+}
+
+uint32_t getInstruction(state *st) {
+//    make sure it's a multiple of 4 (valid alignment)
+    uint32_t pcVal = st->reg[PC];
+    uint32_t instr = 0;
+    for (int i=0; i < 4; i++) {
+        instr |= st->memory[pcVal+i] << i*BITS_IN_BYTE;
+    }
+    return instr;
+//            (st->memory[pcVal] | st->memory[pcVal+1] << BITS_IN_BYTE | st->memory[pcVal+2] << 2 * BITS_IN_BYTE | st->memory[pcVal+3] << 24);
+}
+
+
+int isTerminate(state *st) {
+    return (getInstruction(st)) == 0;
+}
+
+
+
+/* selects the instruction based on bits 27 to 25 and bits 7 to 4 in the case of multiply */
+void selectInstruction(state *st) {
+    uint32_t instr = getInstruction(st);
+    if (bitAccess(isBranch_bit, instr)) {
+        decode_process_data(st);
+        dataProcessing(st);
+    }
+    else if(bitAccess(isSDT_bit,instr)) {
+        decode_single_data_transfer(st);
+        singleDataTransfer(st);
+    }
+    else if(!bitAccess(isMult_bit,instr) && isMult(instr)) {
+        decode_multiply(st);
+        multiply(st);
+    }
+    else {
+        decode_branch(st);
+        branch(st);
+    }
+}
+
+//int main(void) {
+//	uint32_t instruction = 0x0110A0E3;
+//	uint32_t inp2 = 0x022081E2;
+//	uint32_t inp3 = 0x910203E0;
+//	getCond(instruction);
+//	getOpcode(instruction);
+//	getCond(inp2);
+//	getOpcode(inp2);
+//	getCond(inp3);
+//	isMult(inp3);
+//	return 0;
+//}
