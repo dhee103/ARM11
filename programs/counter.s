f516e9b1ee20aecc02c7ea41b7cce722a89ac778
Alex Beaven-Justice <alex.beaven-justice15@imperial.ac.uk>
2016-06-09 16:15:22 +0100

    Updated counter.s

--- programs/counter.s
+++ programs/counter.s
@@ -1,8 +1,7 @@
-setup:
-
+;setup
 ldr r0,=0x20200000
 mov r1,#1
-
+mov r3,#0 ;serves no purpose but to identify counter register
 ;22,27,17 outs
 lsl r1,#21
 str r1,[r0,#4]
@@ -14,24 +13,31 @@ str r2,[r0,#8]
 ;23,24 ins
 
 loop:
-;read input(change), update counter, display count, wait, loop
+;read input and branch to up or down
 ldr r1,[r0, #0x34]
 mov r2,#1
 lsl r2,#23
 tst r1,r2
-be on
+be up
+lsl r2,#1
+tst r1,r2
+be down
 b loop
 
-on:
-mov r1,#1
+
+up:
+mov r1,#1		;just testing first read
 lsl r1,#22         ;r1 is used to write to set and clear addresses
 str r1,[r0,#0x1c]         ;clear pin 16
 
-mov r0,=0x1000
-loop1:
-sub r0,r0,#1
-cmp r0,#0
-bne loop1
+add r3,#1
 
-str r1,[r0,#x28]
+b update
+
+down:
+sub r3,#1
+b update
+
+update:
+//adjust leds
 b loop
